016dd824ff157d16282217a6fea7050d2f4dcf6d
n3oney <neo@neoney.dev>
2023-05-22 23:06:29 +0200

    refactor: improve discord theme

--- configs/discord/themes.nix
+++ configs/discord/themes.nix
@@ -4,28 +4,33 @@
   ...
 }:
 with lib;
-with builtins; {
+with builtins; let
+  background = "#${config.programs.foot.settings.colors.background}${toHexString (floor (config.programs.foot.settings.colors.alpha * 255))}";
+in {
   xdg.configFile."WebCord/Themes/transparency".text = ''
-    body{
-        background-color: transparent;
-    }
+    @import url("https://catppuccin.github.io/discord/dist/catppuccin-macchiato-pink.theme.css");
 
     #app-mount{
-        background-color: transparent;
-        border-color: transparent;
+      background-color: transparent;
+      border-color: transparent;
     }
 
-    .container-2o3qEW, .chat-2ZfjoI, .bg-1QIAus, .app-2CXKsg, .member-2gU6Ar, .sidebar-1tnWFu {
-        background: transparent !important;
+    body, .container-2o3qEW, .chat-2ZfjoI, .bg-1QIAus, .app-2CXKsg, .member-2gU6Ar, .sidebar-1tnWFu, .container-YkUktl, .scroller-WSmht3, .container-1oAagU, .scrollableContainer-15eg7h {
+      background: transparent !important;
     }
 
-    .members-3WRCEx, .container-1NXEtd, .guilds-2JjMmN, .chatContent-3KubbW  {
-        background: #${config.programs.foot.settings.colors.background}${toHexString (floor (config.programs.foot.settings.colors.alpha * 255))} !important;
+    .members-3WRCEx, .container-1NXEtd, .guilds-2JjMmN, .chatContent-3KubbW, .title-31SJ6t, .panels-3wFtMD, .privateChannels-oVe7HL, .searchBar-3TnChZ, .container-2cd8Mz, .nowPlayingColumn-1eCBCN, .itemCard-3Etziu, .container-ZMc96U, .searchBar-2aylmZ, .channelTextArea-1FufC0 {
+      background: ${background} !important;
     }
+
+    .form-3gdLxP::before {
+      opacity: 0;
+    };
+
 
     .container-1NXEtd {
-      border-right: 1px solid #3d3f45;
-      border-left: 1px solid #3d3f45;
+      border-right: 1px solid #3d3f45 !important;
+      border-left: 1px solid #3d3f45 !important;
     }
   '';
 }
