dbdc30b1c2f742bc9c68dd420bcb3b474b96abac
n3oney <neo@neoney.dev>
2023-06-13 19:00:17 +0200

    refactor: use anyrun HM module

--- configs/anyrun.nix
+++ configs/anyrun.nix
@@ -1,9 +1,6 @@
 {
   inputs = {
-    anyrun = {
-      url = "github:kirottu/anyrun";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    anyrun.url = "github:notashelf/anyrun";
   };
 
   system = _: {
@@ -16,97 +13,78 @@
     };
   };
 
+  add = {anyrun, ...}: {
+    homeModules = [anyrun.homeManagerModules.default];
+  };
+
   home = {
     inputs,
     pkgs,
     config,
+    lib,
     ...
   }: {
-    home.packages = [inputs.anyrun.packages.${pkgs.system}.anyrun];
+    programs.anyrun = {
+      enable = true;
 
-    xdg.configFile."anyrun/config.ron".text = ''
-      Config(
-        // `width` and `vertical_offset` use an enum for the value it can be either:
-        // Absolute(n): The absolute value in pixels
-        // Fraction(n): A fraction of the width or height of the full screen (depends on exclusive zones and the settings related to them) window respectively
+      config = {
+        verticalOffset.fraction = 0.3;
+        closeOnClick = true;
+        hidePluginInfo = true;
+        showResultsImmediately = true;
+        plugins = [
+          "libapplications.so"
+          "librink.so"
+          "libtranslate.so"
+        ];
+      };
 
-        // How wide the input box and results are.
-        width: Absolute(800),
 
-        // Where Anyrun is located on the screen: Top, Center
-        position: Top,
+      extraCss = ''
+        window {
+          background: transparent; /* rgba(0, 0, 0, 0.8);*/
+        }
 
-        // How much the runner is shifted vertically
-        vertical_offset: Fraction(0.3),
+        #match,
+        #entry,
+        #plugin,
+        #main {
+          background: transparent;
+        }
 
-        // Hide match and plugin info icons
-        hide_icons: false,
+        #match.activatable {
+          padding: 6px;
+          border-radius: 12px;
+          color: white;
+          margin-top: 4px;
+        }
 
-        // ignore exclusive zones, f.e. Waybar
-        ignore_exclusive_zones: false,
+        #match.activatable:selected {
+          background: #${config.colorScheme.colors.accent};
+          color: black;
+        }
 
-        close_on_click: true,
+        #match-title, #match-desc {
+          color: inherit;
+        }
 
-        // Layer shell layer: Background, Bottom, Top, Overlay
-        layer: Overlay,
+        #entry {
+          color: white;
+          box-shadow: none;
+          border-radius: 12px;
+          border: 2px solid #${config.colorScheme.colors.accent};
+        }
 
-        // Hide the plugin info panel
-        hide_plugin_info: true,
+        box#main {
+          background: rgba(36, 39, 58, 0.7);
+          border-radius: 16px;
+          padding: 8px;
+        }
 
-        show_results_immediately: true,
-
-        plugins: [
-          "libapplications.so",
-          "librink.so",
-          "libtranslate.so",
-        ],
-      )
-    '';
-
-    xdg.configFile."anyrun/style.css".text = ''
-      window {
-        background: transparent; /* rgba(0, 0, 0, 0.8);*/
-      }
-
-      #match,
-      #entry,
-      #plugin,
-      #main {
-        background: transparent;
-      }
-
-      #match.activatable {
-        padding: 6px;
-        border-radius: 12px;
-        color: white;
-        margin-top: 4px;
-      }
-
-      #match.activatable:selected {
-        background: #${config.colorScheme.colors.accent};
-        color: black;
-      }
-
-      #match-title {
-        color: inherit;
-      }
-
-      #entry {
-        color: white;
-        box-shadow: none;
-        border-radius: 12px;
-        border: 2px solid #${config.colorScheme.colors.accent};
-      }
-
-      box#main {
-        background: rgba(36, 39, 58, 0.7);
-        border-radius: 16px;
-        padding: 8px;
-      }
-
-      row:first-child {
-        margin-top: 6px;
-      }
-    '';
+        row:first-child {
+          margin-top: 6px;
+        }
+      '';
+    };
   };
 }
