e3e81dda7d045a0ae0944a0b4f7ad6e2288a2f8a
n3oney <neo@neoney.dev>
2023-06-14 20:43:50 +0200

    style: reformat code

--- configs/archive-team-warrior.nix
+++ configs/archive-team-warrior.nix
@@ -2,13 +2,15 @@
   system = _: {
     virtualisation.docker.enable = true;
 
-    virtualisation.oci-containers.backend = "docker";
-    virtualisation.oci-containers.containers = {
-      archive-team-warrior = {
-        image = "atdr.meo.ws/archiveteam/reddit-grab";
-        autoStart = true;
-        cmd = ["--concurrent=4" "neoney"];
-        extraOptions = ["--network=host"];
+    virtualisation.oci-containers = {
+      backend = "docker";
+      containers = {
+        archive-team-warrior = {
+          image = "atdr.meo.ws/archiveteam/reddit-grab";
+          autoStart = true;
+          cmd = ["--concurrent=4" "neoney"];
+          extraOptions = ["--network=host"];
+        };
       };
     };
   };
