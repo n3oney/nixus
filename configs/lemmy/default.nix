289e51ff971c50258dc143af1e06ab646414d0fb
n3oney <neo@neoney.dev>
2023-06-17 01:14:15 +0200

    chore: better comment on e-mail

--- configs/lemmy/default.nix
+++ configs/lemmy/default.nix
@@ -18,22 +18,9 @@
       enable = true;
       settings = {
         hostname = "lemmy.neoney.dev";
-        # gotta config this in the future
-        # but I don't think this is doable
-        # unless I can load stmp_password
-        # from an environment variable or file
-        # email = {
-        #   # Hostname and port of the smtp server
-        #   smtp_server = "localhost:25";
-        #   # Login name for smtp server
-        #   smtp_login = "string";
-        #   # Password to login to the smtp server
-        #   smtp_password = "string";
-        #   # Address to send emails from, eg "noreply@your-instance.com"
-        #   smtp_from_address = "noreply@example.com";
-        #   # Whether or not smtp connections should use tls. Can be none, tls, or starttls
-        #   tls_type = "none";
-        # };
+        # TODO: Setup e-mail when
+        # https://github.com/LemmyNet/lemmy/pull/3154
+        # lands
       };
       caddy.enable = true;
     };
