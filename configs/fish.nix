c77793588dee1d6d37c08a55c740f5a3cc6e9fdd
n3oney <neo@neoney.dev>
2023-06-16 17:22:22 +0200

    fix: add missing parentheses

--- configs/fish.nix
+++ configs/fish.nix
@@ -21,41 +21,42 @@
         enable_transience
       '';
       functions =
-        if config.programs.foot.enable
-        then let
-          # make terminal opaque while the command is running, and transparent after it stops
-          opaquewrap = binary:
-            with builtins;
-            with lib; ''
-              printf "\033]11;rgba:24/27/3a/ff\007"
+        (
+          if config.programs.foot.enable
+          then let
+            # make terminal opaque while the command is running, and transparent after it stops
+            opaquewrap = binary:
+              with builtins;
+              with lib; ''
+                printf "\033]11;rgba:24/27/3a/ff\007"
 
-              ${binary} $argv
+                ${binary} $argv
 
-              printf "\033]11;rgba:${
-                concatStringsSep "/" (match "([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})" config.programs.foot.settings.colors.background)
-              }/${toHexString (floor (config.programs.foot.settings.colors.alpha * 255))}\007"
-            '';
-        in {
-          nvim =
-            if config.programs.neovim-flake.enable
-            then opaquewrap "command nvim"
-            else null;
-          hx =
-            if config.programs.helix.enable
-            then opaquewrap "command hx"
-            else null;
-          btop =
-            if config.programs.btop.enable
-            then opaquewrap "command btop"
-            else null;
-        }
-        else
-          {}
-          // {
-            hd = ''
-              sudo nix system apply ~/nixus $argv
-            '';
-          };
+                printf "\033]11;rgba:${
+                  concatStringsSep "/" (match "([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})" config.programs.foot.settings.colors.background)
+                }/${toHexString (floor (config.programs.foot.settings.colors.alpha * 255))}\007"
+              '';
+          in {
+            nvim =
+              if config.programs.neovim-flake.enable
+              then opaquewrap "command nvim"
+              else null;
+            hx =
+              if config.programs.helix.enable
+              then opaquewrap "command hx"
+              else null;
+            btop =
+              if config.programs.btop.enable
+              then opaquewrap "command btop"
+              else null;
+          }
+          else {}
+        )
+        // {
+          hd = ''
+            sudo nix system apply ~/nixus $argv
+          '';
+        };
       shellAbbrs = {
         npm = "pnpm";
         cd = "z";
