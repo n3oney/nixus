73a135665328db2a395dedce1cc6eb83be7c93fc
n3oney <neo@neoney.dev>
2023-06-18 17:37:55 +0200

    fix: enable ucode

--- systems/desktop/hardware-configuration.nix
+++ systems/desktop/hardware-configuration.nix
@@ -42,5 +42,5 @@
   networking.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode = true;
 }
