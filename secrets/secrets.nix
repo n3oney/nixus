b01dccf93d5896b0e8fadab7a956ac2845a5b815
n3oney <neo@neoney.dev>
2023-06-19 17:10:02 +0200

    feat(zia): add zia keys to agenix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,12 @@
 let
-  neoney = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHRyeXj99ydcyzJtpZoZ5nMz0oOU97tIZMsygWecUtOk";
+  nixus_pc_user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHRyeXj99ydcyzJtpZoZ5nMz0oOU97tIZMsygWecUtOk";
+  zia_user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILEbP6w13AG23+e8GFD8fXH6c/VgIYKgoJ3D1xEZgNwF";
 
   nixus_pc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICO6PmYOYdJT8IDzgbWp8oHo3h4KCfg9AGjty8fRn/QK";
+  zia = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAAkd9xLNVJa3F4EoQiZICAgDKbjSYwLdzsupsDhUcJm";
 
-  systems = [nixus_pc];
+  users = [nixus_pc_user zia_user];
+  systems = [nixus_pc zia];
 in {
-  "ha_assist_config.age".publicKeys = [neoney] ++ systems;
+  "ha_assist_config.age".publicKeys = users ++ systems;
 }
