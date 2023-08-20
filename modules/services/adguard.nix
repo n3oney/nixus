0f7a853052595c453a0f3d762fb1a3b7715ad44c
n3oney <neo@neoney.dev>
2023-08-20 16:46:11 +0200

    feat: enable adguard's DHCP

--- modules/services/adguard.nix
+++ modules/services/adguard.nix
@@ -14,15 +14,21 @@
   config.os = lib.mkIf config.services.adguard.enable {
     services.adguardhome = {
       enable = true;
+      allowDHCP = true;
     };
 
     networking.firewall.allowedTCPPorts = [
       53
       80
+      3000
     ];
 
     networking.firewall.allowedUDPPorts = [
       53
+      68
+      67
+      547
+      546
     ];
 
     services.caddy = {
