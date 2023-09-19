8b450759edcab113f10681f8f51c53bd72122edd
n3oney <neo@neoney.dev>
2023-09-19 17:04:59 +0200

    feat: update helix to latest version

--- modules/programs/helix/default.nix
+++ modules/programs/helix/default.nix
@@ -1,21 +1,30 @@
 {
   lib,
   pkgs,
-  hmConfig,
+  inputs,
   ...
 }: {
+  inputs.helix.url = "github:helix-editor/helix";
+
   hm = {
     home.sessionVariables.EDITOR = "hx";
 
     programs.helix = {
       enable = true;
-      package = pkgs.helix.overrideAttrs (old: {
-        patches =
-          (old.patches or [])
-          ++ [
-            ./format-filename.patch
-          ];
-      });
+      package = let
+        packages = inputs.helix.packages.${pkgs.system};
+      in
+        packages.helix.passthru.wrapper (packages.helix-unwrapped.overrideAttrs (old: {
+          patches =
+            (old.patches or [])
+            ++ [
+              # doesnt apply for no reason idk y
+              # (pkgs.fetchpatch {
+              # url = "https://github.com/helix-editor/helix/pull/7474.diff";
+              # hash = "sha256-iK9vfYFd6NiMjuhvUyyYA5sb+9bytdF+hTqurwnFk2Y=";
+              # })
+            ];
+        }));
       settings = {
         theme = "catppuccin_macchiato";
 
@@ -34,12 +43,13 @@
       };
 
       languages = {
-        # future-proofing
         language-server = {
+          typst-lsp.command = "${pkgs.typst-lsp}/bin/typst-lsp";
           typescript-language-server = {
             command = lib.getExe pkgs.nodePackages.typescript-language-server;
             args = ["--stdio"];
             config.hostInfo = "helix";
+            config.documentFormatting = false;
           };
           vscode-css-language-server = {
             command = lib.getExe pkgs.nodePackages.vscode-css-languageserver-bin;
@@ -47,18 +57,42 @@
             config.provideFormatter = true;
           };
           nil.command = lib.getExe pkgs.nil;
+          eslint = {
+            command = "${pkgs.vscode-langservers-extracted}/bin/vscode-eslint-language-server";
+            args = ["--stdio"];
+            config = {
+              validate = "on";
+              experimental.useFlatConfig = false;
+              rulesCustomizations = [];
+              run = "onType";
+              problems.shortenToSingleLine = false;
+              nodePath = "";
+              codeAction.disableRuleComment = {
+                enable = true;
+                location = "separateLine";
+              };
+
+              codeActionOnSave = {
+                enable = true;
+                mode = "fixAll";
+              };
+
+              workingDirectory.mode = "location";
+            };
+          };
         };
         language = let
+          withLangServers = lang: servers:
+            lang
+            // {
+              language-servers = (lang.language-servers or []) ++ servers;
+            };
           mkPrettier = name: ext: {
             inherit name;
             auto-format = true;
             formatter = {
-              command = pkgs.writeShellScript "prettierd.sh" ''
-                ${pkgs.prettierd}/bin/prettierd "$1" 2>/dev/null
-
-                ${pkgs.wakatime}/bin/wakatime-cli --entity "$1" --plugin "Helix/${hmConfig.programs.helix.package.version} Helix/${hmConfig.programs.helix.package.version}" --write --key $(cat "${hmConfig.age.secrets.wakatime.path}") --api-url https://wakapi.dev/api 2>&1 >/dev/null &
-              '';
-              args = ["{}"];
+              command = "${pkgs.prettierd}/bin/prettierd";
+              args = ["file.${ext}"];
             };
           };
         in [
@@ -66,50 +100,13 @@
             name = "nix";
             auto-format = true;
             formatter = {command = lib.getExe pkgs.alejandra;};
-            language-server = {command = lib.getExe pkgs.nil;};
           }
-          ((mkPrettier "typescript" "ts")
-            // {
-              language-server = {
-                command = lib.getExe pkgs.nodePackages.typescript-language-server;
-                args = ["--stdio"];
-              };
-            })
-          ((mkPrettier "tsx" "tsx")
-            // {
-              language-server = {
-                command = lib.getExe pkgs.nodePackages.typescript-language-server;
-                args = ["--stdio"];
-              };
-            })
-          ((mkPrettier "javascript" "js")
-            // {
-              language-server = {
-                command = lib.getExe pkgs.nodePackages.typescript-language-server;
-                args = ["--stdio"];
-              };
-            })
-          ((mkPrettier "jsx" "js")
-            // {
-              language-server = {
-                command = lib.getExe pkgs.nodePackages.typescript-language-server;
-                args = ["--stdio"];
-              };
-            })
-          ((mkPrettier "css" "css")
-            // {
-              language-server = {
-                command = lib.getExe pkgs.nodePackages.vscode-css-languageserver-bin;
-                args = ["--stdio"];
-              };
-            })
-          ((mkPrettier "scss" "scss")
-            // {
-              language-server = {
-                command = lib.getExe pkgs.nodePackages.vscode-css-languageserver-bin;
-                args = ["--stdio"];
-              };
-            })
+          (withLangServers (mkPrettier "typescript" "ts") ["typescript-language-server" "eslint"])
+          (withLangServers (mkPrettier "tsx" "tsx") ["typescript-language-server" "eslint"])
+          (withLangServers (mkPrettier "javascript" "js") ["typescript-language-server" "eslint"])
+          (withLangServers (mkPrettier "jsx" "js") ["typescript-language-server" "eslint"])
+          (mkPrettier "css" "css")
+          (mkPrettier "scss" "scss")
           (mkPrettier "markdown" "md")
         ];
       };
