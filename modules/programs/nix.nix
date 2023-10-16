3fd5eb515f1f656c05b22e1fb906ce14d69bfdfa
n3oney <neo@neoney.dev>
2023-10-16 14:22:08 +0200

    feat: add nix settings to home manager too

--- modules/programs/nix.nix
+++ modules/programs/nix.nix
@@ -3,10 +3,60 @@
   lib,
   osConfig,
   ...
-}: {
+}: let
+  nixSettings = {
+    # Free up to 20GiB whenever there is less than 5GB left.
+    # this setting is in bytes, so we multiply with 1024 thrice
+    min-free = "${toString (5 * 1024 * 1024 * 1024)}";
+    max-free = "${toString (20 * 1024 * 1024 * 1024)}";
+    # automatically optimise symlinks
+    auto-optimise-store = true;
+
+    builders-use-substitutes = true;
+
+    keep-going = true;
+    # show more log lines for failed builds
+    log-lines = 30;
+
+    max-jobs = "auto";
+    sandbox = true;
+
+    warn-dirty = false;
+
+    # maximum number of parallel TCP connections used to fetch imports and binary caches, 0 means no limit
+    http-connections = 0;
+
+    keep-derivations = true;
+    keep-outputs = true;
+
+    accept-flake-config = true;
+
+    extra-experimental-features = ["flakes" "nix-command" "recursive-nix" "ca-derivations"];
+
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+      "https://hyprland.cachix.org"
+      "https://cache.privatevoid.net"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "cache.privatevoid.net:SErQ8bvNWANeAvtsOESUwVYr2VJynfuc9JRwlzTTkVg="
+    ];
+
+    trusted-users = [
+      "root"
+      "@wheel"
+    ];
+  };
+in {
   inputs = {
     nix-super.url = "github:privatevoid-net/nix-super";
   };
+
+  hm.nix.settings = nixSettings;
 
   os = {
     nixpkgs.overlays = [
@@ -43,51 +93,7 @@
         options = "--delete-older-than 3d";
       };
 
-      settings = {
-        # Free up to 20GiB whenever there is less than 5GB left.
-        # this setting is in bytes, so we multiply with 1024 thrice
-        min-free = "${toString (5 * 1024 * 1024 * 1024)}";
-        max-free = "${toString (20 * 1024 * 1024 * 1024)}";
-        # automatically optimise symlinks
-        auto-optimise-store = true;
-
-        builders-use-substitutes = true;
-
-        keep-going = true;
-        # show more log lines for failed builds
-        log-lines = 30;
-
-        max-jobs = "auto";
-        sandbox = true;
-
-        warn-dirty = false;
-
-        # maximum number of parallel TCP connections used to fetch imports and binary caches, 0 means no limit
-        http-connections = 0;
-
-        keep-derivations = true;
-        keep-outputs = true;
-
-        accept-flake-config = true;
-
-        extra-experimental-features = ["flakes" "nix-command" "recursive-nix" "ca-derivations"];
-
-        substituters = [
-          "https://nix-community.cachix.org"
-          "https://hyprland.cachix.org"
-          "https://cache.privatevoid.net"
-        ];
-        trusted-public-keys = [
-          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-          "cache.privatevoid.net:SErQ8bvNWANeAvtsOESUwVYr2VJynfuc9JRwlzTTkVg="
-        ];
-
-        trusted-users = [
-          "root"
-          "@wheel"
-        ];
-      };
+      settings = nixSettings;
     };
   };
 
