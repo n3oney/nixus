4aa9d35708efbce4b8dab6b0057b48fa2ccb60a4
n3oney <neo@neoney.dev>
2023-12-29 00:08:34 +0100

    feat: add packages to ags's path

--- modules/programs/ags/default.nix
+++ modules/programs/ags/default.nix
@@ -14,8 +14,17 @@
   config.hmModules = [inputs.ags.homeManagerModules.default];
 
   config.hm = lib.mkIf config.programs.ags.enable {
-    programs.ags = {
+    programs.ags = let
+      agsPathPackages = [pkgs.sassc pkgs.wlroots.examples pkgs.bash pkgs.procps pkgs.findutils];
+    in {
       enable = true;
+
+      package = inputs.ags.packages.${pkgs.system}.default.overrideAttrs (_: {
+        postFixup = ''
+          wrapProgram $out/bin/ags \
+            --prefix PATH : "${builtins.concatStringsSep ":" (builtins.map (v: "${v}/bin") agsPathPackages)}"
+        '';
+      });
 
       # configDir = ./config;
 
