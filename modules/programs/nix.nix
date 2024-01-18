8485830e9d09d0511a3e54235b5f106024978188
n3oney <neo@neoney.dev>
2024-01-18 19:55:49 +0100

    fix: remove privatevoid binary cache

--- modules/programs/nix.nix
+++ modules/programs/nix.nix
@@ -38,13 +38,13 @@
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
-      "https://cache.privatevoid.net"
+      # "https://cache.privatevoid.net"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "cache.privatevoid.net:SErQ8bvNWANeAvtsOESUwVYr2VJynfuc9JRwlzTTkVg="
+      # "cache.privatevoid.net:SErQ8bvNWANeAvtsOESUwVYr2VJynfuc9JRwlzTTkVg="
     ];
 
     trusted-users = [
