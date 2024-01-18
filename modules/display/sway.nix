84a10d0d36a382c0cd3efa23467bbe67caba6a4f
n3oney <neo@neoney.dev>
2024-01-18 19:58:38 +0100

    feat: make sway config depend on hyprland

--- modules/display/sway.nix
+++ modules/display/sway.nix
@@ -2,9 +2,11 @@
   config,
   lib,
   pkgs,
+  hmConfig,
   ...
 }: let
   cfg = config.display;
+  hlCfg = hmConfig.wayland.windowManager.hyprland.settings;
 in {
   options.display.sway.enable = lib.mkEnableOption "sway";
 
@@ -62,6 +64,52 @@ in {
         };
 
         bars = [];
+
+        input = let
+          hlDevices = lib.filterAttrs (n: _: lib.hasPrefix "device:" n) hlCfg;
+          swayDevices = lib.mapAttrs' (n: v:
+            lib.nameValuePair (builtins.substring 7 (builtins.stringLength n) n) ({
+                pointer_accel = builtins.toString (v.sensitivity or 0);
+              }
+              // (
+                if v ? kb_layout
+                then {xkb_layout = v.kb_layout;}
+                else {}
+              )
+              // (
+                if v ? kb_model
+                then {xkb_model = v.kb_model;}
+                else {}
+              )
+              // (
+                if v ? kb_rules
+                then {xkb_rules = v.kb_rules;}
+                else {}
+              )))
+          hlDevices;
+        in
+          swayDevices
+          // {
+            "*" = let
+              i = hlCfg.input;
+            in {
+              xkb_options = i.kb_options;
+              accel_profile = i.accel_profile;
+              pointer_accel = builtins.toString i.sensitivity;
+              dwt =
+                if i.touchpad.disable_while_typing
+                then "enable"
+                else "disable";
+              drag_lock =
+                if i.touchpad.drag_lock
+                then "enable"
+                else "disable";
+              click_method =
+                if i.touchpad.clickfinger_behavior
+                then "clickfinger"
+                else "none";
+            };
+          };
       };
 
       extraConfig = ''
@@ -77,7 +125,26 @@ in {
         default_border pixel
         default_floating_border pixel
 
-        layer_effects 'bar-0' blur enable; blur_ignore_transparent enable
+        ### Layerrule config
+        ${builtins.concatStringsSep "\n" (builtins.filter (elem: elem != null) (builtins.map (rule: let
+            elems = builtins.split "," rule;
+            layer = builtins.elemAt elems 2;
+            hlRule = builtins.elemAt elems 0;
+
+            swayRule =
+              {
+                blur = "blur";
+                ignorezero = "blur_ignore_transparent";
+                ignorealpha = "blur_ignore_transparent";
+                noanim = null;
+              }
+              ."${builtins.elemAt (builtins.split " " hlRule) 0}";
+          in (
+            if swayRule == null
+            then null
+            else "layer_effects '${layer}' ${swayRule} enable"
+          ))
+          hlCfg.layerrule))}
       '';
     };
   };
