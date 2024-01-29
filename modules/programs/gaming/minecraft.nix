6d35a9ee0e5f4eabf89b23d9fb125c5f307c0bc0
n3oney <neo@neoney.dev>
2024-01-29 19:09:51 +0100

    feat: use wayland in minecraft

--- modules/programs/gaming/minecraft.nix
+++ modules/programs/gaming/minecraft.nix
@@ -7,6 +7,6 @@
   options.programs.gaming.minecraft.enable = lib.mkEnableOption "Minecraft";
 
   config.hm = lib.mkIf config.programs.gaming.minecraft.enable {
-    home.packages = [pkgs.prismlauncher];
+    home.packages = [pkgs.prismlauncher pkgs.glfw-wayland-minecraft];
   };
 }
