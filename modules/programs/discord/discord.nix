92c09461a034bc4c78fc679c0b32a4cacec3111c
n3oney <neo@neoney.dev>
2024-06-23 10:50:24 +0200

    fix: update discord

--- modules/programs/discord/discord.nix
+++ modules/programs/discord/discord.nix
@@ -16,10 +16,12 @@ in {
       readOnly = true;
       default = cfg.package.overrideAttrs (old: {
         patches = (old.patches or []) ++ [./readonlyFix.patch];
-        postFixup = ''
-          wrapProgram $out/bin/${cfg.package.meta.mainProgram or (lib.getName cfg.package)} \
-            --add-flags "--enable-features=UseOzonePlatform --ozone-platform=wayland"
-        '';
+        postFixup =
+          (old.postFixup or "")
+          + ''
+            wrapProgram $out/bin/${cfg.package.meta.mainProgram or (lib.getName cfg.package)} \
+              --add-flags "--enable-features=UseOzonePlatform --ozone-platform=wayland"
+          '';
       });
     };
   };
