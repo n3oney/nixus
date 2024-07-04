dbeac716d851d5d556608c706add966dad008ab5
n3oney <neo@neoney.dev>
2024-07-04 14:30:40 +0200

    feat: yubikey touch detector

--- modules/services/yubikey-touch-detector.nix
+++ modules/services/yubikey-touch-detector.nix
@@ -9,6 +9,6 @@
   config.hm.systemd.user.services.yubikey-touch-detector = lib.mkIf config.services.yubikey-touch-detector.enable {
     Unit.Description = "YubiKey touch detector";
     Install.WantedBy = ["graphical-session.target"];
-    Service.ExecStart = lib.getExe pkgs.yubikey-touch-detector;
+    Service.ExecStart = "${lib.getExe pkgs.yubikey-touch-detector} --libnotify";
   };
 }
