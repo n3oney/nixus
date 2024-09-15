8a37889a020c1dfcb6582f3698ff6ed692228a97
n3oney <neo@neoney.dev>
2024-09-15 11:52:44 +0200

    chore: update orcaslicer

--- modules/programs/orcaslicer.nix
+++ modules/programs/orcaslicer.nix
@@ -8,17 +8,17 @@
 
   config.hm = lib.mkIf config.programs.orcaSlicer.enable (let
     orcaslicer = let
-      version = "2.1.1";
+      version = "2.2.0-beta";
     in (pkgs.appimageTools.wrapType2 {
       pname = "orca-slicer";
       inherit version;
 
       src = pkgs.fetchurl {
-        url = "https://github.com/SoftFever/OrcaSlicer/releases/download/v${version}/OrcaSlicer_Linux_V${version}.AppImage";
-        hash = "sha256-kvM1rBGEJhjRqQt3a8+I0o4ahB1Uc9qB+4PzhYoNQdM=";
+        url = "https://github.com/SoftFever/OrcaSlicer/releases/download/v${version}/OrcaSlicer_Linux_Ubuntu2404_V${version}.AppImage";
+        hash = "sha256-jTRfcs84MDbiM6G/sYM9eV8tEGINa1irPGmuFfj+tTM=";
       };
 
-      extraPkgs = pkgs: [pkgs.webkitgtk];
+      extraPkgs = pkgs: [pkgs.webkitgtk_4_1];
 
       profile = ''
         export GST_PLUGIN_SYSTEM_PATH_1_0=/usr/lib/gstreamer-1.0
