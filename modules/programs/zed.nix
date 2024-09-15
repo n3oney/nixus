4ae6d4ca6efc2cf36ef78f9436422be3f1d596db
n3oney <neo@neoney.dev>
2024-09-15 11:52:56 +0200

    feat: add zed settings

--- modules/programs/zed.nix
+++ modules/programs/zed.nix
@@ -15,5 +15,87 @@
     home.packages = [
       (pkgs.zed-editor.override {withGLES = true;})
     ];
+
+    xdg.configFile."zed/settings.json".text = ''
+      {
+        "assistant": {
+          "default_model": {
+            "provider": "anthropic",
+            "model": "claude-3-5-sonnet-20240620"
+          },
+          "version": "2"
+        },
+        "ui_font_size": 16,
+        "buffer_font_size": 16,
+        "theme": {
+          "mode": "system",
+          "light": "Rosé Pine",
+          "dark": "One Dark"
+        },
+        "load_direnv": "direct",
+        "vim_mode": true,
+        "inlay_hints": {
+          "enabled": true,
+          "show_type_hints": false
+        },
+        "journal": {
+          "hour_format": "hour24"
+        },
+        "lsp": {
+          "nixd": {
+            "binary": {
+              "path": "${pkgs.nixd}/bin/nixd"
+            }
+          }
+        },
+        "terminal": {
+          "dock": "right"
+        },
+        "languages": {
+          "JavaScript": {
+            "formatter": {
+              "external": {
+                "command": "${pkgs.nodePackages.prettier}/bin/prettier",
+                "arguments": ["--stdin-filepath", "{buffer_path}"]
+              }
+            },
+            "code_actions_on_format": {
+              "source.fixAll.eslint": true
+            }
+          },
+          "TSX": {
+            "formatter": {
+              "external": {
+                "command": "${pkgs.nodePackages.prettier}/bin/prettier",
+                "arguments": ["--stdin-filepath", "{buffer_path}"]
+              }
+            },
+            "code_actions_on_format": {
+              "source.fixAll.eslint": true
+            }
+          },
+          "TypeScript": {
+            "formatter": {
+              "external": {
+                "command": "${pkgs.nodePackages.prettier}/bin/prettier",
+                "arguments": ["--stdin-filepath", "{buffer_path}"]
+              }
+            },
+            "code_actions_on_format": {
+              "source.fixAll.eslint": true
+            }
+          }
+        },
+        "vim": {
+          "use_system_clipboard": "always",
+          "use_multiline_find": true,
+          "use_smartcase_find": true
+        },
+        "relative_line_numbers": true,
+        "scrollbar": {
+          "show": "never"
+        }
+      }
+    '';
   };
 }
