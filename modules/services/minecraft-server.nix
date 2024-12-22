c2deb110b335c483551ad2ec46369b1fc453e811
n3oney <neo@neoney.dev>
2024-12-22 01:31:06 +0100

    chore: update lockfile

--- modules/services/minecraft-server.nix
+++ modules/services/minecraft-server.nix
@@ -12,7 +12,6 @@
     eula = true;
     whitelist = {
       neoney = "22c31a0b-3251-4fc4-a9ef-f0697d5735b0";
-      xRirty = "4bcb8a8e-fd25-4824-b4a1-c587ce53ae1f";
     };
     openFirewall = true;
     serverProperties = {
