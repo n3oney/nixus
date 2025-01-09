efd72ddf8c4bde0f067787618b0d99938f3688d4
n3oney <neo@neoney.dev>
2025-01-09 03:32:19 +0100

    chore: update nur import

--- modules/misc/nur.nix
+++ modules/misc/nur.nix
@@ -8,7 +8,7 @@
 
   config = lib.mkMerge [
     (lib.mkIf config.nur.enable {
-      os.nixpkgs.overlays = [inputs.nur.overlay];
+      os.nixpkgs.overlays = [inputs.nur.overlays.default];
     })
   ];
 }
