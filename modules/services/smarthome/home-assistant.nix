775c095b7640418e037e68a3cd02aa22ab9db189
n3oney <neo@neoney.dev>
2025-02-09 12:51:29 +0100

    chore: update home-assistant

--- modules/services/smarthome/home-assistant.nix
+++ modules/services/smarthome/home-assistant.nix
@@ -25,7 +25,7 @@
       containers.homeassistant = {
         volumes = ["/etc/home-assistant:/config" "/var/lib/home-assistant-media:/media"];
         environment.TZ = "Europe/Warsaw";
-        image = "ghcr.io/home-assistant/home-assistant:2024.12";
+        image = "ghcr.io/home-assistant/home-assistant:2025.1";
         extraOptions = [
           "--network=host"
         ];
