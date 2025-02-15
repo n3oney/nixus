11a5418aa5067bb0f7081529d48a587841295a47
n3oney <neo@neoney.dev>
2025-02-15 17:23:40 +0100

    feat: open UDP 25565

--- modules/services/minecraft-server.nix
+++ modules/services/minecraft-server.nix
@@ -6,11 +6,15 @@
 }: {
   options.services.minecraft-server.enable = lib.mkEnableOption "Minecraft Server";
 
-  config.os.services.minecraft-server = lib.mkIf config.services.minecraft-server.enable {
-    package = pkgs.papermcServers.papermc-1_21_3;
-    enable = true;
-    eula = true;
-    openFirewall = true;
-    declarative = false;
+  config.os = lib.mkIf config.services.minecraft-server.enable {
+    services.minecraft-server = {
+      package = pkgs.papermcServers.papermc-1_21_3;
+      enable = true;
+      eula = true;
+      openFirewall = true;
+      declarative = false;
+    };
+
+    networking.firewall.allowedUDPPorts = [25565];
   };
 }
