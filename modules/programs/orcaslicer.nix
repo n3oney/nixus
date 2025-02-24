d7337e91ba79ca7e5aa52d41483e9f23f56403f7
n3oney <neo@neoney.dev>
2025-02-24 10:57:15 +0100

    chore: update orcaslicer

--- modules/programs/orcaslicer.nix
+++ modules/programs/orcaslicer.nix
@@ -8,14 +8,14 @@
 
   config.hm = lib.mkIf config.programs.orcaSlicer.enable (let
     orcaslicer = let
-      version = "2.2.0";
+      version = "2.3.0-beta";
     in (pkgs.appimageTools.wrapType2 {
       pname = "orca-slicer";
       inherit version;
 
       src = pkgs.fetchurl {
-        url = "https://github.com/SoftFever/OrcaSlicer/releases/download/v${version}/OrcaSlicer_Linux_Ubuntu2404_V${version}.AppImage";
-        hash = "sha256-hu373XlBX+m7wfWyjMGZK4IroZBP+ZaeOMTenIT346Q=";
+        url = "https://github.com/SoftFever/OrcaSlicer/releases/download/v${version}/OrcaSlicer_Linux_AppImage_Ubuntu2404_V${version}.AppImage";
+        hash = "sha256-0cokoMUXO/wRO23tzYteTB+vfxpLD+vt1vZ2Vku9lmg=";
       };
 
       extraPkgs = pkgs: [pkgs.webkitgtk_4_1];
