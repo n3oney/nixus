955dbf3a3c72e00822b72eb7562231237ca80f5c
n3oney <neo@neoney.dev>
2025-03-13 14:21:54 +0100

    fix: add nodejs to path for copilot

--- modules/programs/rider.nix
+++ modules/programs/rider.nix
@@ -7,7 +7,18 @@
   options.programs.rider.enable = lib.mkEnableOption "JetBrains Rider";
 
   config = lib.mkIf config.programs.rider.enable {
-    hm.home.packages = [pkgs.jetbrains.rider pkgs.dotnet-sdk_9];
+    hm.home.packages = [
+      (pkgs.jetbrains.rider.overrideAttrs
+        (old: {
+          postFixup =
+            (old.postFixup or "")
+            + ''
+              wrapProgram $out/bin/rider \
+                --suffix PATH : "${pkgs.nodejs_23}/bin"
+            '';
+        }))
+      pkgs.dotnet-sdk_9
+    ];
 
     impermanence.userDirs = [".java/.userPrefs" ".config/JetBrains" ".local/share/JetBrains" ".cache/JetBrains"];
   };
