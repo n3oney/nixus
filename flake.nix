af8245b926161b706b88f0ad49450d2e9e6ec765
n3oney <neo@neoney.dev>
2025-04-25 16:43:13 +0200

    chore: update lockfile

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
     hyprpicker.url = "github:hyprwm/hyprpicker";
     impermanence.url = "github:nix-community/impermanence";
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0-3.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nh = {
