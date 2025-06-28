983f97a073c4802a62fd62fa8fd8e546e2fd53b9
n3oney <neo@neoney.dev>
2025-06-28 11:37:55 +0200

    feat: better keybindings

--- modules/programs/vscode/default.nix
+++ modules/programs/vscode/default.nix
@@ -20,106 +20,121 @@
             '';
         });
         enable = true;
-        userSettings = {
-          "[typescript]" = {
-            "editor.defaultFormatter" = "esbenp.prettier-vscode";
-          };
-          "[typescriptreact]" = {
-            "editor.defaultFormatter" = "esbenp.prettier-vscode";
-          };
-          "[javascript]" = {
-            "editor.defaultFormatter" = "esbenp.prettier-vscode";
-          };
-          "[jsonc]" = {
-            "editor.defaultFormatter" = "esbenp.prettier-vscode";
-          };
-          "[json]" = {
-            "editor.defaultFormatter" = "esbenp.prettier-vscode";
-          };
-          "editor.fontLigatures" = "'calt', 'ss01', 'ss02', 'ss03', 'ss04', 'ss06', 'ss07', 'ss08', 'ss09', 'liga'";
-          "git.enableSmartCommit" = true;
-          "git.autofetch" = false;
-          "git.confirmSync" = false;
-          "editor.fontFamily" = "monospace";
-          "editor.fontSize" = 15;
-          "editor.formatOnSave" = true;
+        profiles.default = {
+          keybindings = [
+            {
+              key = "ctrl+p";
+              command = "-extension.vim_ctrl+p";
+              when = "editorTextFocus && vim.active && vim.use<C-p> && !inDebugRepl || vim.active && vim.use<C-p> && !inDebugRepl && vim.mode == 'CommandlineInProgress' || vim.active && vim.use<C-p> && !inDebugRepl && vim.mode == 'SearchInProgressMode'";
+            }
+            {
+              key = "ctrl+f";
+              command = "workbench.action.quickOpen";
+              args = "%";
+            }
+          ];
+          userSettings = {
+            "[typescript]" = {
+              "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            };
+            "[typescriptreact]" = {
+              "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            };
+            "[javascript]" = {
+              "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            };
+            "[jsonc]" = {
+              "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            };
+            "[json]" = {
+              "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            };
+            "editor.fontLigatures" = "'calt', 'ss01', 'ss02', 'ss03', 'ss04', 'ss06', 'ss07', 'ss08', 'ss09', 'liga'";
+            "git.enableSmartCommit" = true;
+            "git.autofetch" = false;
+            "git.confirmSync" = false;
+            "editor.fontFamily" = "monospace";
+            "editor.fontSize" = 15;
+            "editor.formatOnSave" = true;
 
-          "editor.codeActionsOnSave" = {
-            "source.organizeImports.biome" = "explicit";
+            "editor.codeActionsOnSave" = {
+              "source.organizeImports.biome" = "explicit";
+            };
+
+            "workbench.colorTheme" = "nightfall";
+            "git.openRepositoryInParentFolders" = "always";
+            "security.workspace.trust.enabled" = false;
+            "typescript.inlayHints.parameterNames.enabled" = "all";
+
+            "nix.formatterPath" = "${pkgs.alejandra}/bin/alejandra";
+            "nix.enableLanguageServer" = true;
+            "nix.serverPath" = "${pkgs.nil}/bin/nil";
+            "nix.serverSettings" = {
+              "nil.formatting.command" = ["${pkgs.alejandra}/bin/alejandra"];
+            };
+            "github.copilot.nextEditSuggestions.enabled" = true;
+            "vim.useSystemClipboard" = true;
+            "vim.easymotion" = true;
           };
 
-          "workbench.colorTheme" = "nightfall";
-          "git.openRepositoryInParentFolders" = "always";
-          "security.workspace.trust.enabled" = false;
-          "typescript.inlayHints.parameterNames.enabled" = "all";
-
-          "nix.formatterPath" = "${pkgs.alejandra}/bin/alejandra";
-          "nix.enableLanguageServer" = true;
-          "nix.serverPath" = "${pkgs.nil}/bin/nil";
-          "nix.serverSettings" = {
-            "nil.formatting.command" = ["${pkgs.alejandra}/bin/alejandra"];
-          };
-          "github.copilot.nextEditSuggestions.enabled" = true;
-          "vim.useSystemClipboard" = true;
+          extensions = with pkgs;
+            (with vscode-extensions; [
+              biomejs.biome
+              vscodevim.vim
+              mkhl.direnv
+              esbenp.prettier-vscode
+              dbaeumer.vscode-eslint
+              usernamehw.errorlens
+              eamodio.gitlens
+              github.vscode-github-actions
+              editorconfig.editorconfig
+              bradlc.vscode-tailwindcss
+              jnoortheen.nix-ide
+              rust-lang.rust-analyzer
+              github.copilot
+              github.copilot-chat
+            ])
+            ++ (
+              pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+                {
+                  name = "nightfall";
+                  publisher = "nightfall";
+                  version = "0.0.3";
+                  sha256 = "sha256-E7BXc9P3RKIXTh3Vb6/r6SJIdfvJB3puXDd6FuQDr9w=";
+                }
+                {
+                  name = "effect-vscode";
+                  publisher = "effectful-tech";
+                  version = "0.1.7";
+                  sha256 = "sha256-qLTqOxQCTIn/tzbTU51D/XpOYnPn1M1/8UNv30t9hDE=";
+                }
+                {
+                  name = "tailwind-fold";
+                  publisher = "stivo";
+                  version = "0.2.0";
+                  sha256 = "sha256-yH3eA5jgBwxqnpFQkg91KQMkQps5iM1v783KQkQcWUU=";
+                }
+                {
+                  name = "sqltools";
+                  publisher = "mtxr";
+                  version = "0.28.3";
+                  sha256 = "sha256-bTrHAhj8uwzRIImziKsOizZf8+k3t+VrkOeZrFx7SH8=";
+                }
+                {
+                  name = "vscode-monorepo-workspace";
+                  publisher = "folke";
+                  version = "1.3.1";
+                  sha256 = "sha256-BtJBd9T+5qPY2YD93Rn2AiClwYGD1s4o+IEUHdaq61c=";
+                }
+                {
+                  name = "pretty-ts-errors";
+                  publisher = "yoavbls";
+                  version = "0.5.4";
+                  sha256 = "sha256-SMEqbpKYNck23zgULsdnsw4PS20XMPUpJ5kYh1fpd14=";
+                }
+              ]
+            );
         };
-
-        extensions = with pkgs;
-          (with vscode-extensions; [
-            biomejs.biome
-            vscodevim.vim
-            mkhl.direnv
-            esbenp.prettier-vscode
-            dbaeumer.vscode-eslint
-            usernamehw.errorlens
-            eamodio.gitlens
-            github.vscode-github-actions
-            editorconfig.editorconfig
-            bradlc.vscode-tailwindcss
-            jnoortheen.nix-ide
-            rust-lang.rust-analyzer
-            github.copilot
-            github.copilot-chat
-          ])
-          ++ (
-            pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-              {
-                name = "nightfall";
-                publisher = "nightfall";
-                version = "0.0.3";
-                sha256 = "sha256-E7BXc9P3RKIXTh3Vb6/r6SJIdfvJB3puXDd6FuQDr9w=";
-              }
-              {
-                name = "effect-vscode";
-                publisher = "effectful-tech";
-                version = "0.1.7";
-                sha256 = "sha256-qLTqOxQCTIn/tzbTU51D/XpOYnPn1M1/8UNv30t9hDE=";
-              }
-              {
-                name = "tailwind-fold";
-                publisher = "stivo";
-                version = "0.2.0";
-                sha256 = "sha256-yH3eA5jgBwxqnpFQkg91KQMkQps5iM1v783KQkQcWUU=";
-              }
-              {
-                name = "sqltools";
-                publisher = "mtxr";
-                version = "0.28.3";
-                sha256 = "sha256-bTrHAhj8uwzRIImziKsOizZf8+k3t+VrkOeZrFx7SH8=";
-              }
-              {
-                name = "vscode-monorepo-workspace";
-                publisher = "folke";
-                version = "1.3.1";
-                sha256 = "sha256-BtJBd9T+5qPY2YD93Rn2AiClwYGD1s4o+IEUHdaq61c=";
-              }
-              {
-                name = "pretty-ts-errors";
-                publisher = "yoavbls";
-                version = "0.5.4";
-                sha256 = "sha256-SMEqbpKYNck23zgULsdnsw4PS20XMPUpJ5kYh1fpd14=";
-              }
-            ]
-          );
       };
     };
   };
