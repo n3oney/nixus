36a971ae0513ffef4fbffefe6911b1aea19edd64
n3oney <neo@neoney.dev>
2025-08-26 09:24:56 +0200

    fix(swappy): remove patch that got merged

--- modules/programs/swappy.nix
+++ modules/programs/swappy.nix
@@ -7,14 +7,6 @@
   options.programs.swappy.enable = lib.mkEnableOption "swappy";
 
   config.hm.home.packages = lib.mkIf config.programs.swappy.enable [
-    (pkgs.swappy.overrideAttrs (old: {
-      patches = [
-        (pkgs.fetchpatch
-          {
-            url = "https://patch-diff.githubusercontent.com/raw/jtheoof/swappy/pull/151.patch";
-            hash = "sha256-j7AyhEWoAoJhWjksPix36HpcuGDCFfk+/bV1CA2jgyM=";
-          })
-      ];
-    }))
+    pkgs.swappy
   ];
 }
