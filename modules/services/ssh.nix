789687d33ad35e0eaee4ffe2ddb2cf92a85ceb45
n3oney <neo@neoney.dev>
2025-09-07 13:50:39 +0200

    fix: ssh deprecation warning

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -16,7 +16,18 @@
   hm = {
     programs.ssh = {
       enable = true;
-      addKeysToAgent = "yes";
+      matchBlocks."*" = {
+        forwardAgent = false;
+        addKeysToAgent = "yes";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
+      };
       extraConfig = ''
         Include hosts
       '';
