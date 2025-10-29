f078362561c59d1ffd3c92d71a93d444ee3a3abd
n3oney <neo@neoney.dev>
2025-10-29 10:36:13 +0100

    refactor: switch to orca-slicer stable

--- modules/programs/orca-slicer/default.nix
+++ modules/programs/orca-slicer/default.nix
@@ -7,8 +7,8 @@
   options.programs.orcaSlicer.enable = lib.mkEnableOption "Orca";
 
   config = lib.mkIf config.programs.orcaSlicer.enable (let
-    orcaslicer = pkgs.orca-slicer-nightly;
-    # orcaslicer = pkgs.orca-slicer;
+    # orcaslicer = pkgs.orca-slicer-nightly;
+    orcaslicer = pkgs.orca-slicer;
   in {
     impermanence.userDirs = [".config/OrcaSlicer" ".local/share/orca-slicer"];
 
