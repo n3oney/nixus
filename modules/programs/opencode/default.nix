b08f9ed9af47833fcb99002ce8b489ac20b420a8
n3oney <neo@neoney.dev>
2025-11-23 20:31:49 +0100

    chore: update and patch opencode

--- modules/programs/opencode/default.nix
+++ modules/programs/opencode/default.nix
@@ -15,7 +15,9 @@
 
     hm = {
       programs.opencode = {
-        package = inputs.nix-ai-tools.packages.${pkgs.system}.opencode;
+        package = inputs.nix-ai-tools.packages.${pkgs.system}.opencode.overrideAttrs (old: {
+          patches = (old.patches or []) ++ [./0000-opencode.patch];
+        });
         enable = true;
         settings = {
           theme = "system";
