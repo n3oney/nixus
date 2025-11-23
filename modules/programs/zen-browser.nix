25fa971d4bd52e5b391c628c14820fca82f361ac
n3oney <neo@neoney.dev>
2025-11-23 20:31:49 +0100

    fix: widevine

--- modules/programs/zen-browser.nix
+++ modules/programs/zen-browser.nix
@@ -9,15 +9,15 @@
 
   config = lib.mkIf config.programs.zenBrowser.enable (let
     zenBrowser = inputs.zen-browser-flake.packages.${pkgs.system}.zen-browser.overrideAttrs (old: {
-      buildCommand =
-        old.buildCommand
-        + ''
-          mkdir -p $out/gmp-widevinecdm/system-installed
-          ln -s "${pkgs.widevine-cdm}/share/google/chrome/WidevineCdm/_platform_specific/linux_arm64/libwidevinecdm.so" $out/gmp-widevinecdm/system-installed/libwidevinecdm.so
-          ln -s "${pkgs.widevine-cdm}/share/google/chrome/WidevineCdm/manifest.json" $out/gmp-widevinecdm/system-installed/manifest.json
-          wrapProgram "$oldExe" \
-                  --set MOZ_GMP_PATH "$out/gmp-widevinecdm/system-installed"
-        '';
+      # buildCommand =
+      #   old.buildCommand
+      #   + ''
+      #     mkdir -p $out/gmp-widevinecdm/system-installed
+      #     ln -s "${pkgs.widevine-cdm}/share/google/chrome/WidevineCdm/_platform_specific/linux_arm64/libwidevinecdm.so" $out/gmp-widevinecdm/system-installed/libwidevinecdm.so
+      #     ln -s "${pkgs.widevine-cdm}/share/google/chrome/WidevineCdm/manifest.json" $out/gmp-widevinecdm/system-installed/manifest.json
+      #     wrapProgram "$oldExe" \
+      #             --set MOZ_GMP_PATH "$out/gmp-widevinecdm/system-installed"
+      #   '';
     });
   in {
     impermanence.userDirs = [".zen"];
