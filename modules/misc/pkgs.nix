0b8241f03295abc6a5d1c641491f0d0221f683d3
n3oney <neo@neoney.dev>
2025-12-27 23:28:08 +0100

    chore: windows reboot script

--- modules/misc/pkgs.nix
+++ modules/misc/pkgs.nix
@@ -1,3 +1,9 @@
-_: {
+{pkgs, ...}: {
   os.nixpkgs.overlays = [(import ../../pkgs/overlays)];
+
+  os.environment.systemPackages = [
+    (pkgs.writeShellScriptBin "windows" ''
+      sudo ${pkgs.efibootmgr}/bin/efibootmgr --bootnext 0032 && sudo reboot
+    '')
+  ];
 }
