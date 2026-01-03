89c0a889351717d14a05895c0dde25c30dbfbef8
n3oney <neo@neoney.dev>
2026-01-03 16:43:48 +0100

    chore: fix garnix substituter

--- modules/programs/nix.nix
+++ modules/programs/nix.nix
@@ -34,11 +34,6 @@
 
     extra-experimental-features = ["flakes" "nix-command"];
 
-    extra-substituters = ["https://cache.garnix.io/"];
-    extra-trusted-public-keys = [
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-    ];
-
     substituters = [
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
@@ -52,6 +47,7 @@
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       # "cache.privatevoid.net:SErQ8bvNWANeAvtsOESUwVYr2VJynfuc9JRwlzTTkVg="
     ];
 
