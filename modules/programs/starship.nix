ce0e821330d37d0640ea2dfb1b6090bae4ed17b7
n3oney <neo@neoney.dev>
2026-01-09 23:17:17 +0100

    feat: jj starship integration

--- modules/programs/starship.nix
+++ modules/programs/starship.nix
@@ -1,11 +1,11 @@
-_: {
+{pkgs, inputs, ...}: {
   hm.programs.starship = {
     enable = true;
     # enableFishIntegration = true; I'd set it to true, but instead I'm doing it manually, since enable_transience is being broken
     settings = {
       format = ''
-        ([\[](fg:8)$package$rust$username$hostname$cmd_duration$jobs[\]](fg:8))
-        $directory([\[](fg:8)$git_branch$git_state$git_status$git_metrics[\]](fg:8))$fill$status$time
+        ([\[](fg:8)$package$rust$username$hostname$cmd_duration$jobs[\]](fg:8))
+        $directory([\[](fg:8)''${custom.jj}[\]](fg:8))$fill$status$time
         $character
       '';
       scan_timeout = 10;
@@ -20,45 +20,33 @@ _: {
       };
       directory = {
         style = "bold blue";
-        read_only = " ";
+        read_only = " ";
         truncate_to_repo = false;
-        format = "[ $path]($style)[$read_only]($read_only_style) ";
+        format = "[ $path]($style)[$read_only]($read_only_style) ";
         fish_style_pwd_dir_length = 1;
         truncation_length = 1;
         home_symbol = "~";
       };
-      git_branch = {
-        format = " [$symbol$branch]($style) ";
-        symbol = "";
-      };
-      git_status = {
-        format = "([$conflicted$deleted$renamed$modified$staged$untracked$ahead_behind]($style))";
-        style = "bold cyan";
-        up_to_date = "";
-        conflicted = "=$count ";
-        ahead = "⇡$count ";
-        behind = "⇣$count ";
-        diverged = "⇕$count ";
-        untracked = "?$count ";
-        stashed = "$$count ";
-        modified = "!$count ";
-        staged = "+$count ";
-        renamed = "»$count ";
-        deleted = "✘$count ";
-      };
-      git_metrics = {
-        disabled = false;
-        format = "([+$added]($added_style) )([-$deleted]($deleted_style) )";
+      # Disable built-in git modules - jj-starship handles both jj and git
+      git_branch.disabled = true;
+      git_status.disabled = true;
+      git_metrics.disabled = true;
+      git_state.disabled = true;
+      # jj-starship: unified prompt for jj and git repos
+      custom.jj = {
+        when = "jj-starship detect";
+        shell = ["jj-starship"];
+        format = "$output";
       };
       jobs = {
         disabled = false;
-        format = " bg jobs: [$symbol$number]($style) ";
+        format = " bg jobs: [$symbol$number]($style) ";
         number_threshold = 1;
         symbol = "";
       };
       package = {
         format = "[$symbol$version]($style) ";
-        symbol = " ";
+        symbol = " ";
       };
       cmd_duration = {
         min_time = 2000;
@@ -79,4 +67,8 @@ _: {
       };
     };
   };
+
+  hm.home.packages = [
+    inputs.jj-starship.packages.${pkgs.system}.default
+  ];
 }
