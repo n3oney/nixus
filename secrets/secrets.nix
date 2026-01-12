c592578b34da715dd9506e97f1e4a55ecaf797b5
n3oney <neo@neoney.dev>
2026-01-12 22:58:26 +0100

    chore: rekey secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,28 +3,31 @@ let
   ciri_user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKnnJimJIwFnqkC5GTWM3lq6QTfjA8rJldKu4ue3ckih";
   yen_user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJJByA+ZlxOI6VEG+QXqYQu80PtfdNMXRwbERqiC4vUR";
   max_user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINgayxuDLXH6QYqoFJeEqqKo5/6fqfuS4fOYN9TaL6Rj";
+  prism_user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK/6B/hbuNdUQsrIsUUJ6wjT+3umZj8wdcmX9KQ7oiSy";
 
   miko_host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICO6PmYOYdJT8IDzgbWp8oHo3h4KCfg9AGjty8fRn/QK";
   ciri_host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILlkZf59+zbc+mtODZdTZbPpKLFVYxLXbrpzSqWE6XDY";
   yen_host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJuP7PM81YCMmifSPC/tBHEJ4jKI9HAGxAUDqP5PQIiB";
   max_host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO4j2OzWCrqgW/5BieZyloZFxS1dfiC/KO41P5b9iYzM";
+  prism_host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMAUC0JjvU+pLxLmvluh5rfQwQGaQM+y1qJrc/hVOiAr";
 
   miko = [miko_user miko_host];
   ciri = [ciri_user ciri_host];
   yen = [yen_user yen_host];
   max = [max_user max_host];
+  prism = [prism_user prism_host];
 in {
-  "ha_assist_config.age".publicKeys = miko ++ ciri;
-  "gh_notifications_key.age".publicKeys = miko ++ ciri;
-  "cloudflared.age".publicKeys = miko ++ max;
+  "ha_assist_config.age".publicKeys = miko ++ ciri ++ prism;
+  "gh_notifications_key.age".publicKeys = miko ++ ciri ++ prism;
+  "cloudflared.age".publicKeys = miko ++ max ++ prism;
 
-  "ssh_hosts.age".publicKeys = miko ++ ciri ++ yen ++ max;
+  "ssh_hosts.age".publicKeys = miko ++ ciri ++ yen ++ max ++ prism;
 
-  "ngrok.age".publicKeys = miko ++ ciri ++ yen ++ max;
+  "ngrok.age".publicKeys = miko ++ ciri ++ yen ++ max ++ prism;
 
-  "z2m.age".publicKeys = miko ++ ciri ++ max;
+  "z2m.age".publicKeys = miko ++ ciri ++ max ++ prism;
 
-  "mcp.age".publicKeys = miko ++ ciri;
+  "mcp.age".publicKeys = miko ++ ciri ++ prism;
 
-  "librechat.age".publicKeys = miko ++ ciri ++ yen;
+  "librechat.age".publicKeys = miko ++ ciri ++ yen ++ prism;
 }
