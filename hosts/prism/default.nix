61103519d55726028601d4bc0db9ceb170f5600a
n3oney <neo@neoney.dev>
2026-01-13 10:17:30 +0100

    feat: plymouth boot ui

--- hosts/prism/default.nix
+++ hosts/prism/default.nix
@@ -19,7 +19,29 @@
     boot.loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
+      timeout = 3;
     };
+
+    boot.plymouth = {
+      enable = true;
+      theme = "rings";
+      themePackages = [
+        (pkgs.adi1090x-plymouth-themes.override {
+          selected_themes = ["rings"];
+        })
+      ];
+    };
+
+    # Silent boot
+    boot.consoleLogLevel = 3;
+    boot.initrd.verbose = false;
+    boot.kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
+    ];
 
     networking = {
       hostName = "prism";
