aca4baa831947ce191d16b5624a274b4a69c7323
n3oney <neo@neoney.dev>
2026-01-15 23:35:23 +0100

    chore: update lockfile

--- modules/programs/adb.nix
+++ modules/programs/adb.nix
@@ -7,7 +7,7 @@
   options.programs.adb.enable = lib.mkEnableOption "adb";
 
   config.os = lib.mkIf config.programs.adb.enable {
-    environment.systemPackages = [pkgs.android-tools];
     users.users.neoney.extraGroups = ["adbusers"];
+    environment.systemPackages = [pkgs.android-tools];
   };
 }
