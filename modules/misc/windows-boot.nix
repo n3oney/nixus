44e3e2f47ecb491caa2f5b6a77aabd31f5beafc5
n3oney <neo@neoney.dev>
2026-02-09 08:41:19 +0100

    feat: windows reboot keybind

--- modules/misc/windows-boot.nix
+++ modules/misc/windows-boot.nix
@@ -5,6 +5,10 @@
   ...
 }: let
   cfg = config.windowsBoot;
+  
+  windowsScript = pkgs.writeShellScriptBin "windows" ''
+    ${pkgs.efibootmgr}/bin/efibootmgr --bootnext ${cfg.bootEntry} && reboot
+  '';
 in {
   options.windowsBoot = {
     enable = lib.mkEnableOption "Windows boot script";
@@ -13,13 +17,27 @@ in {
       type = lib.types.str;
       description = "EFI boot entry for Windows (e.g., '0032', '0004')";
     };
+    
+    finalPackage = lib.mkOption {
+      type = lib.types.package;
+      readOnly = true;
+      default = windowsScript;
+      description = "The final windows boot script package";
+    };
   };
 
   config = lib.mkIf cfg.enable {
-    os.environment.systemPackages = [
-      (pkgs.writeShellScriptBin "windows" ''
-        sudo ${pkgs.efibootmgr}/bin/efibootmgr --bootnext ${cfg.bootEntry} && sudo reboot
-      '')
+    os.environment.systemPackages = [cfg.finalPackage];
+    
+    os.security.sudo.extraRules = [{
+      commands = [
+        { command = "${cfg.finalPackage}/bin/windows"; options = ["NOPASSWD"]; }
+      ];
+      groups = ["wheel"];
+    }];
+    
+    hm.wayland.windowManager.hyprland.settings.bind = [
+      "SUPER, XF86Launch9, exec, sudo ${cfg.finalPackage}/bin/windows"
     ];
   };
 }
