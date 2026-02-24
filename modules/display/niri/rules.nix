38e2832d232c9ee6404abbe9fcb7fb1701054e87
n3oney <neo@neoney.dev>
2026-02-25 00:16:45 +0100

    fix: handle new application options

--- modules/display/niri/rules.nix
+++ modules/display/niri/rules.nix
@@ -8,13 +8,25 @@
   appsWithWorkspace = lib.filterAttrs (
     _: app: app.defaultWorkspace != null && app.windowClass != null
   ) config.applications;
+  appsWithRules = lib.filterAttrs (
+    _: app: app.windowClass != null && (app.defaultWorkspace != null || app.defaultColumnWidth != null || app.blockFromScreencast)
+  ) config.applications;
 in {
   config = mkIf cfg.enable {
     hm.programs.niri.settings.window-rules =
-      lib.mapAttrsToList (_: app: {
-        matches = [{app-id = app.windowClass;}];
-        open-on-workspace = toString app.defaultWorkspace;
-      })
-      appsWithWorkspace;
+      lib.mapAttrsToList (_: app:
+        {
+          matches = [{app-id = app.windowClass;}];
+        }
+        // lib.optionalAttrs (app.defaultWorkspace != null) {
+          open-on-workspace = toString app.defaultWorkspace;
+        }
+        // lib.optionalAttrs (app.defaultColumnWidth != null) {
+          default-column-width.proportion = app.defaultColumnWidth;
+        }
+        // lib.optionalAttrs app.blockFromScreencast {
+          block-out-from = "screencast";
+        })
+      appsWithRules;
   };
 }
