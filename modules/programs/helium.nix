33c6805ac64bc9402d04b2fb7ec82987908b0e0d
n3oney <neo@neoney.dev>
2026-03-11 12:47:25 +0100

    feat(helium): set as default in xdg.mime

--- modules/programs/helium.nix
+++ modules/programs/helium.nix
@@ -41,6 +41,19 @@ in {
       ".cache/net.imput.helium"
     ];
 
-    hm.home.packages = [helium];
+    hm = {
+      home.packages = [helium];
+
+      xdg.mimeApps = {
+        enable = true;
+        defaultApplications = {
+          "text/html" = "helium.desktop";
+          "x-scheme-handler/http" = "helium.desktop";
+          "x-scheme-handler/https" = "helium.desktop";
+          "x-scheme-handler/about" = "helium.desktop";
+          "x-scheme-handler/unknown" = "helium.desktop";
+        };
+      };
+    };
   };
 }
