e5adbac15da4b4ea6769c9697234866af4b08ad3
n3oney <neo@neoney.dev>
2026-04-17 17:36:13 +0200

    feat: use resolved on tailscale machines

--- modules/services/tailscale.nix
+++ modules/services/tailscale.nix
@@ -8,9 +8,26 @@
   config = lib.mkIf config.services.tailscale.enable {
     os = {
       services.tailscale.enable = true;
-      networking.nameservers = lib.mkForce ["100.100.100.100" "1.1.1.1" "1.0.0.1"];
-      networking.search = ["forest-piano.ts.net"];
+      # networking.nameservers = lib.mkForce ["100.100.100.100" "1.1.1.1" "1.0.0.1"];
+      # networking.search = ["forest-piano.ts.net"];
       networking.firewall.allowedUDPPorts = [41641];
+
+      services.resolved.enable = true;
+      networking.networkmanager.dns = "systemd-resolved";
+
+      # Set Cloudflare as the system-wide upstream
+      services.resolved = {
+        fallbackDns = ["1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001"];
+        dnssec = "false";
+        domains = ["~."]; # route all otherwise-unmatched queries to the configured DNS
+
+        settings = {
+          Resolve = {
+            DNS = "1.1.1.1 1.0.0.1 2606:4700:4700::1111 2606:4700:4700::1001";
+            DNSStubListener = "yes";
+          };
+        };
+      };
     };
     impermanence.systemDirs = ["/var/lib/tailscale"];
   };
