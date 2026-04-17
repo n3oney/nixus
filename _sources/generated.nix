e838ddccf36315b62bdc08f0a80f9e6b48f75ef8
n3oney <neo@neoney.dev>
2026-04-17 17:36:13 +0200

    chore: update helium

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -8,10 +8,10 @@
 {
   helium = {
     pname = "helium";
-    version = "0.12.1.1";
+    version = "0.12.5.1";
     src = fetchurl {
-      url = "https://github.com/imputnet/helium-linux/releases/download/0.12.1.1/helium-0.12.1.1-x86_64.AppImage";
-      sha256 = "sha256-+UE+JqQtxbA5szPvAohapXlES21VBOdNsV6Ej1dRRfs=";
+      url = "https://github.com/imputnet/helium-linux/releases/download/0.12.5.1/helium-0.12.5.1-x86_64.AppImage";
+      sha256 = "sha256-uUZauNralX6katmnO9VDLEs+d+HIhkjkeV36Dw2eUmM=";
     };
   };
   home-assistant = {
