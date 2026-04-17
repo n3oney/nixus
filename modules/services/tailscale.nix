fd03fbe8aa3c8632ba680064b957c458e004287e
n3oney <neo@neoney.dev>
2026-04-17 17:36:13 +0200

    refactor: cleanup tailscale config

--- modules/services/tailscale.nix
+++ modules/services/tailscale.nix
@@ -12,22 +12,17 @@
       # networking.search = ["forest-piano.ts.net"];
       networking.firewall.allowedUDPPorts = [41641];
 
-      services.resolved.enable = true;
-      networking.networkmanager.dns = "systemd-resolved";
-
-      # Set Cloudflare as the system-wide upstream
       services.resolved = {
-        fallbackDns = ["1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001"];
-        dnssec = "false";
-        domains = ["~."]; # route all otherwise-unmatched queries to the configured DNS
-
-        settings = {
-          Resolve = {
-            DNS = "1.1.1.1 1.0.0.1 2606:4700:4700::1111 2606:4700:4700::1001";
-            DNSStubListener = "yes";
-          };
+        enable = true;
+        settings.Resolve = {
+          DNS = "1.1.1.1 1.0.0.1 2606:4700:4700::1111 2606:4700:4700::1001";
+          DNSStubListener = "yes";
+          FallbackDNS = ["1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001"];
+          DNSSEC = "false";
+          Domains = ["~."]; # route all otherwise-unmatched queries to the configured DNS
         };
       };
+      networking.networkmanager.dns = "systemd-resolved";
     };
     impermanence.systemDirs = ["/var/lib/tailscale"];
   };
