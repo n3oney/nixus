fe49b1f7dd14862c57e5a2ea7492596ac335b521
n3oney <neo@neoney.dev>
2026-04-17 17:36:13 +0200

    chore: update niri

--- flake.nix
+++ flake.nix
@@ -43,13 +43,13 @@
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    niri-blur = {
-      url = "github:niri-wm/niri/wip/branch";
+    niri = {
+      url = "github:niri-wm/niri";
       flake = false;
     };
     niri-flake = {
       url = "github:sodiboo/niri-flake";
-      inputs.niri-unstable.follows = "niri-blur";
+      inputs.niri-unstable.follows = "niri";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
